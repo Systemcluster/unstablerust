068bee823dbff20cc1509481ee49289f91e84116
Systemcluster <me@systemcluster.me>
2023-02-26 08:20:07 +0100

    Use server-component root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+/** @jsxImportSource react */
 
 import '../styles/reset.css'
 import '../styles/global.css'
@@ -8,35 +8,47 @@ import { Fragment, ReactNode } from 'react'
 
 import { AnalyticsWrapper } from '@/components/analytics'
 
+export const metadata = {
+    title: {
+        default: 'Unstable Rust Feature Explorer',
+        template: '%s | Unstable Rust Feature Explorer',
+    },
+    description: 'Explore and compare the unstable features between versions of the Rust programming language.',
+    keywords: 'rust, programming, nightly rust, rust features, rust nightly, feature comparison, versions, unstable features',
+    authors: [
+        {
+            name: 'Systemcluster',
+        },
+    ],
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover',
+    icons: [
+        {
+            rel: 'icon',
+            url: '/favicon.svg',
+            type: 'image/svg+xml',
+        },
+        {
+            rel: 'apple-touch-icon',
+            url: '/favicon.svg',
+            type: 'image/svg+xml',
+        },
+    ],
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        noarchive: true,
+    },
+    themeColor: '#030303',
+    other: {
+        darkreader: 'disable',
+        'X-UA-Compatible': 'IE=edge',
+    },
+}
+
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
-            <head>
-                <meta charSet="utf8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
-                />
-                <meta name="theme-color" content="#030303" />
-                <title>Unstable Rust Feature Explorer</title>
-                <meta
-                    name="description"
-                    content="Explore and compare the unstable features between versions of the Rust programming language."
-                />
-                <meta
-                    name="keywords"
-                    content="rust, programming, nightly rust, rust features, rust nightly,
-                    feature comparison, versions, unstable features"
-                />
-                <meta name="author" content="Systemcluster" />
-
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
-                <link rel="icon" type="image/svg" href="/favicon.svg" />
-                <link rel="apple touch icon" href="/favicon.svg" />
-
-                <meta name="darkreader" content="disable" />
-            </head>
             <Fragment>
                 {children}
                 <AnalyticsWrapper />
