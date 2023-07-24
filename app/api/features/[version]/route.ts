15593f0bb1e45785a405c2ade8408426453efd9c
Systemcluster <me@systemcluster.me>
2023-07-24 07:56:51 +0200

    Adjust cache-control headers

--- app/api/features/[version]/route.ts
+++ app/api/features/[version]/route.ts
@@ -1,16 +1,19 @@
 import { NextResponse, NextRequest } from 'next/server'
 
+export const runtime = 'edge'
+
 export async function GET(request: NextRequest, { params }: { params: any }): Promise<NextResponse> {
     try {
         const request = await fetch(`https://doc.rust-lang.org/${params.version}/unstable-book/the-unstable-book.html`)
         const book = await request.text()
+        const maxAge = params.version === 'nightly' || params.version === 'beta' ? 60 * 15 : 60 * 60 * 24 * 7
         return NextResponse.json(
             { raw: book },
             {
                 headers: {
-                    'Cache-Control': `max-age=${
-                        params.version === 'nightly' || params.version === 'beta' ? 60 * 15 : 60 * 60 * 24 * 7
-                    }, public`,
+                    'Cache-Control': `public, max-age=${maxAge}, s-maxage=${maxAge}`,
+                    'CDN-Cache-Control': `public, s-maxage=${maxAge}`,
+                    'Vercel-CDN-Cache-Control': `public, s-maxage=${maxAge}`,
                 },
             }
         )
