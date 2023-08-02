b0707ca0ea09ad1ea0ea868e1021493bc2042a26
Systemcluster <me@systemcluster.me>
2023-08-02 06:43:27 +0200

    Adjust server-side data caching

--- app/api/features/[version]/route.ts
+++ app/api/features/[version]/route.ts
@@ -4,9 +4,11 @@ export const runtime = 'edge'
 
 export async function GET(request: NextRequest, { params }: { params: any }): Promise<NextResponse> {
     try {
-        const request = await fetch(`https://doc.rust-lang.org/${params.version}/unstable-book/the-unstable-book.html`)
-        const book = await request.text()
         const maxAge = params.version === 'nightly' || params.version === 'beta' ? 60 * 15 : 60 * 60 * 24 * 7
+        const request = await fetch(`https://doc.rust-lang.org/${params.version}/unstable-book/the-unstable-book.html`, {
+            cache: 'no-store',
+        })
+        const book = await request.text()
         return NextResponse.json(
             { raw: book },
             {
