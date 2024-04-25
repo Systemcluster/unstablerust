fbb06790fc741b61f19df20aeebf7eb3feac5962
Systemcluster <me@systemcluster.me>
2024-04-25 15:08:31 +0200

    Cache feature details

--- app/api/feature/[version]/[...url]/route.ts
+++ app/api/feature/[version]/[...url]/route.ts
@@ -1,16 +1,21 @@
 import { NextResponse, NextRequest } from 'next/server'
 
+export const runtime = 'edge'
+
 export async function GET(request: NextRequest, { params }: { params: any }): Promise<NextResponse> {
     try {
-        const request = await fetch(`https://doc.rust-lang.org/${params.version}/unstable-book/${params.url.join('/')}`)
+        const maxAge = params.version === 'nightly' || params.version === 'beta' ? 60 * 15 : 60 * 60 * 24 * 7
+        const request = await fetch(`https://doc.rust-lang.org/${params.version}/unstable-book/${params.url.join('/')}`, {
+            cache: 'no-store',
+        })
         const book = await request.text()
         return NextResponse.json(
             { raw: book },
             {
                 headers: {
-                    'Cache-Control': `max-age=${
-                        params.version === 'nightly' || params.version === 'beta' ? 60 * 120 : 60 * 60 * 24 * 7
-                    }, public`,
+                    'Cache-Control': `public, max-age=${maxAge}, s-maxage=${maxAge}`,
+                    'CDN-Cache-Control': `public, s-maxage=${maxAge}`,
+                    'Vercel-CDN-Cache-Control': `public, s-maxage=${maxAge}`,
                 },
             }
         )
